b9ccd036e676bffedfc86a976b392a91e1b4fee0
patiphanz <patiphan.sr@ku.th>
2019-05-02 15:04:21 +0700

    first commit

---
+++ consts/firebase_info.js
@@ -0,0 +1,31 @@
+export const config = {
+  apiKey: "AIzaSyCevd68Iq6QcCdjialuc48N8G5XJl4Wa7I",
+  authDomain: "myapp-85961.firebaseapp.com",
+  databaseURL: "https://myapp-85961.firebaseio.com",
+  projectId: "myapp-85961",
+  storageBucket: "myapp-85961.appspot.com",
+  messagingSenderId: "45807384634"
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export const config = {
+//   apiKey: "AIzaSyDaY9hbGQOr41ljQeZgX26NTd_SqKwH4oQ",
+//   authDomain: "studentloan-7fbce.firebaseapp.com",
+//   databaseURL: "https://studentloan-7fbce.firebaseio.com",
+//   projectId: "studentloan-7fbce",
+//   storageBucket: "studentloan-7fbce.appspot.com",
+//   messagingSenderId: "1016135551882"
+// };
